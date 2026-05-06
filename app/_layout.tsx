a15b8e3ad607adecfeb495186b93bc1060123041
Abir-7 <md.tazwarul.islam.07@gmail.com>
2026-05-06 23:25:03 +0600

    create some page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(admin)" options={{ headerShown: false }} />
         <Stack.Screen name="(user)" options={{ headerShown: false }} />
       </Stack>
